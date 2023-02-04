04792aafd1759e050cd46773622952afc26882fe
BhanuPrakashPebbeti <pebbetibhanu2017@gmail.com>
2023-02-04 22:51:31 +0530

    Added db conn, schema

--- server/app.js
+++ server/app.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+
 const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
 const PORT = 5000;
+const url = "mongodb://localhost:27017/tracker"
+
+const connectDB = (url) => {
+
+    mongoose.connect(url,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log(`DB Connection successful`);
+    }).catch((err) => {
+        console.log(`DB not connected`);
+        console.log(err);
+    });
+}
 
 const start = async () => {
     try{
-        // connectDB(url);
+        connectDB(url);
         app.listen(PORT,()=>{
             console.log(`Server is running at port no ${PORT}`);
         });
---
+++ server/model/driverSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const driverSchema = new mongoose.Schema(
+  {
+    longitude: [
+      {
+        type: mongoose.Schema.Types.Decimal128,
+      },
+    ],
+    latitude: [
+      {
+        type: mongoose.Schema.Types.Decimal128,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Driver = mongoose.model("Driver", driverSchema);
+
+module.exports = Driver;
